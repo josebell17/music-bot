d2b212ef84ee1dd873d2fc3724d330ba89966693
josebell17 <locbuifake@gmail.com>
2026-08-30 12:46:41 +0700

    Update bot_langngo.py

--- bot_langngo.py
+++ bot_langngo.py
@@ -26,6 +26,7 @@ YTDL_OPTIONS = {
     'nocheckcertificate': True,
     'source_address': '0.0.0.0',
     'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    'cookiefile': '/etc/secrets/cookies.txt'
 }
 
 FFMPEG_OPTIONS = {
